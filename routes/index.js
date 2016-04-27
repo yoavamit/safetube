bbabbc54d15cb40a1f0475485667f60328944d3e
Yoav Amit <yoav.amit@safebreach.com>
2016-04-27 12:10:14 +0300

    Downloading youtube videos

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var youtubedl = require('youtube-dl');
+var fs = require('fs');
 
-/* GET home page. */
+var YOUTUBE = 'http://www.youtube.com/watch?v=';
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+  // expecting something like this: BJd7F8sAkJ0 (the video ID in youtube)
+  var videoLink = req.query.video;
+  console.log(videoLink);
+  var video = youtubedl(YOUTUBE + videoLink, ['--format=18'], {cmd: __dirname});
+  video.on('info', function(info) {
+    console.log('Download started');
+    console.log('filename: ' + info.filename);
+    console.log('size: ' + info.size);
+  });
+  video.on('complete', function complete(info) {
 
-router.get('/video')
+  });
+  video.on('end', function() {
+    console.log('finished downloading!');
+    res.render('index', { title: 'Express' });
+  });
+  video.pipe(fs.createWriteStream(videoLink + '.mp4'));
+});
 
 module.exports = router;
